3e0edcf2b17d3ffe667ce81bde4c8552bb7915fa
hussainabbas2000 <hussainabbas2000@gmail.com>
2025-05-13 01:35:01 -0700

    second commit

--- app.py
+++ app.py
@@ -59,4 +59,5 @@ def get_bulk_pricing():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port = port, debug=True)
